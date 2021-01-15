a03ad21da74e9378d546e33efdffdfeca05a29c9
RomanRazumov <sakton@mail.ru>
2021-01-15 23:04:59 +0300

    pr

--- Algorithms/FastPow/main.cpp
+++ Algorithms/FastPow/main.cpp
@@ -9,4 +9,22 @@ int fastPow( int a, int n ) {
   }
 }
 
-int main( ) { std::cout << fastPow( 2, 10 ); }
+int fastPowIterative( int a, int n ) {
+  if ( n == 0 ) return 1;
+  int res = 1;
+  while ( n ) {
+    if ( n % 2 ) {
+      res *= a;
+      --n;
+    } else {
+      a *= a;
+      n /= 2;
+    }
+  }
+  return res;
+}
+
+int main( ) {
+  std::cout << fastPow( 2, 10 ) << std::endl;
+  std::cout << fastPowIterative( 2, 10 ) << std::endl;
+}
